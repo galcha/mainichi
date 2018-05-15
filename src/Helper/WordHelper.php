27469b9b0dec7b41a5a9af4428a02260f6ec9d25
Galcha <galcha51@hotmail.fr>
2018-05-15 14:12:19 +0200

    Format response

--- src/Helper/WordHelper.php
+++ src/Helper/WordHelper.php
@@ -25,12 +25,34 @@ class WordHelper {
     public function getWordOfTheDay()
     {
         if (!$this->cache->has('word')) {
+            /* @TODO : allow other APIs */
             $words = json_decode($this->client->request('GET', 'http://jisho.org/api/v1/search/words?keyword=jlpt-n5')->getBody()->getContents());
-            $randomKey = random_int(0, count($words->data) - 1);
+            $words = $this->formatJishoResponse($words);
+            $randomKey = random_int(0, count($words) - 1);
 
-            $this->cache->set('word', $words->data[$randomKey], strtotime('tomorrow') - time());
+            $this->cache->set('word', $words[$randomKey], strtotime('tomorrow') - time());
         }
 
         return $this->cache->get('word');
+    }
+
+    /**
+     * @param object $response
+     * @return array
+     */
+    private function formatJishoResponse($response)
+    {
+        $return = [];
+
+        foreach($response->data as $word) {
+            $return[] = [
+                'original' => $word->japanese[0]->word,
+                'originalReading' => $word->japanese[0]->reading,
+                'translation' => $word->senses[0]->english_definitions,
+                'wordType' => $word->senses[0]->parts_of_speech,
+            ];
+        }
+
+        return $return;
     }
 }
